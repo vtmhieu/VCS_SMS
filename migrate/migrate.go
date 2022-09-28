987913de566ab1f161ea12c95887867e9ecc7c82
vtmhieu <vtmhieu111@gmail.com>
2022-09-29 00:10:51 +0700

    mai

---
+++ migrate/migrate.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/vtmhieu/VCS_SMS/initializers"
+	"github.com/vtmhieu/VCS_SMS/models"
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("🚀 Could not load environment variables", err)
+	}
+
+	initializers.ConnectDB(&config)
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.Server{})
+	fmt.Println("👍 Migration complete")
+}
+
+//we loaded the environment variables and created a connection pool to the Postgres database in the init() function
+//Then, we evoked the AutoMigrate() function provided by GORM to create the database migration and push the changes to the database.
---
+++ models/server.model.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Server struct {
+	Server_id    uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key" json:"server_id,omitempty"`
+	Server_name  string    `gorm:"uniqueIndex;not null" json:"server_name,omitempty"`
+	Status       string    `gorm:"not null" json:"status,omitempty"`
+	Created_time time.Time `gorm:"not null" json:"created_time,omitempty"`
+	Last_updated time.Time `gorm:"not null" json:"last_updated,omitempty"`
+	Ipv4         string    `gorm:"not null" json:"ipv4,omitempty"`
+}
+
+type Create_server struct {
+	Server_id    uuid.UUID `json:"server_id" binding:"required"`
+	Server_name  string    `json:"server_name" binding:"required"`
+	Status       string    `json:"status" binding:"required"`
+	Created_time time.Time `json:"created_time,omitempty"`
+	Last_updated time.Time `json:"last_updated,omitempty"`
+	Ipv4         string    `json:"ipv4,omitempty"`
+}
+
+type Update_server struct {
+	Server_id    uuid.UUID `json:"server_id,omitempty"`
+	Server_name  string    `json:"server_name,omitempty"`
+	Status       string    `json:"status,omitempty"`
+	Created_time time.Time `json:"created_time,omitempty"`
+	Last_updated time.Time `json:"last_updated,omitempty"`
+	Ipv4         string    `json:"ipv4,omitempty"`
+}
