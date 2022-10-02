d3264d95f44b50146a3160230392d36e0f05ae4f
vtmhieu <vtmhieu111@gmail.com>
2022-10-02 22:40:18 +0700

    s

--- main.go
+++ main.go
@@ -31,7 +31,7 @@ func main() {
 
 	router := server.Group("/api")
 	router.GET("/healthchecker", func(ctx *gin.Context) {
-		message := "Welcome to Golang with Gorm and Postgres"
+		message := "Welcome to VCS Server Management System"
 		ctx.JSON(http.StatusOK, gin.H{"status": "success", "message": message})
 	})
 
